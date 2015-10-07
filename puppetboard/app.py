5e26137b2e01b708982db8dbaee3e75404943191
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-10-07 16:02:08 +0200

    Debug EPFL inventory

--- puppetboard/app.py
+++ puppetboard/app.py
@@ -318,6 +318,7 @@ def epfl_inventory():
     virt_contract_count = 0
     phys_non_contract_count = 0
     virt_non_contract_count = 0
+    other_count = 0
     total_count = 0
 
     for node in nodelist:
@@ -330,6 +331,8 @@ def epfl_inventory():
             phys_non_contract_count += 1
         elif (factvalues[node,'contract'] == 'false' and factvalues[node,'is_virtual'] == 'true'):
             virt_non_contract_count += 1
+        else:
+            other_count += 1
 
     return Response(stream_with_context(
         stream_template('epfl_inventory.html', nodedata=nodedata, fact_desc=fact_desc, phys_contract_count=phys_contract_count, virt_contract_count=virt_contract_count, phys_non_contract_count=phys_non_contract_count,virt_non_contract_count=virt_non_contract_count,total_count=total_count)))
